22272abffb93cba99ac3bc17e0aa08716cbeaf29
sriley <sarahriley.dev@gmail.com>
2020-01-08 17:06:07 -0500

    absoluteValuesSumMinimization

---
+++ absoluteValuesSumMinimization.js
@@ -0,0 +1,6 @@
+function absoluteValuesSumMinimization(a) {
+  //     looking for the median number
+  return a.length % 2 === 0 ? a[a.length / 2 - 1] : a[Math.floor(a.length / 2)];
+}
+
+absoluteValuesSumMinimization([2, 4, 7]);
